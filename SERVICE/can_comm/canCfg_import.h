1659a1c74a4592126a6530cc944806bc3b5a6f96
Xjianjian <39685781+Xjianjian@users.noreply.github.com>
2020-09-19 11:19:31 +0800

    Create canCfg_import.h

---
+++ SERVICE/can_comm/canCfg_import.h
@@ -0,0 +1,144 @@
+
+#ifndef CAN_CFG_IMPORT_H_
+#define CAN_CFG_IMPORT_H_
+
+/*------------------------------------------------------------------------------
+ 					Include files
+------------------------------------------------------------------------------*/
+#include "importTypes.h"
+
+/*------------------------------------------------------------------------------
+                             Defines and constants
+------------------------------------------------------------------------------*/
+
+
+
+               
+/*------------------------------------------------------------------------------
+                                   Data types
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+                      External declaration of global RAM-Variables
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+                      External declaration of global ROM-Variables
+------------------------------------------------------------------------------*/
+
+
+
+
+/*------------------------------------------------------------------------------
+                    External declaration of global EEPROM-Variables
+------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------
+                              Declaration of global functions
+------------------------------------------------------------------------------*/
+extern void PT_CAN_0x731_msg_rx_cb(void);
+
+extern void PT_CAN_0x7E7_msg_rx_cb(void);
+
+extern void PT_CAN_0x7DF_msg_rx_cb(void);
+
+extern void PT_CAN_0x632_msg_rx_cb(void);
+
+extern void PT_CAN_0x634_msg_rx_cb(void);
+
+extern void PT_CAN_0x637_msg_rx_cb(void);
+
+extern void PT_CAN_0x63E_msg_rx_cb(void);
+
+
+extern void PT_CAN_0x302_do_ctrl_msg_rx_cb(void);
+
+extern void PT_CAN_0x303_do_ctrl_msg_rx_cb(void);
+
+extern void PT_CAN_0x304_do_ctrl_msg_rx_cb(void);
+extern void PT_CAN_0x3A6_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x3F3_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x492_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x394_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x407_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x487_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x075_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x07E_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x514_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x574_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x102_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x103_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x104_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x112_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x113_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x114_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x126_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x134_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x2C1_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x309_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x312_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x313_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x354_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x387_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x38D_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x38E_po_ctrl_msg1_rx_cb(void);
+
+extern void C_CAN_0x39E_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x4FE_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x222_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x232_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x058_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x3FE_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x0A5_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x122_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x124_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x142_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x144_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x1BE_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x1C5_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x1D5_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x212_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x219_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x21A_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x242_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x252_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x272_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x282_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x2A1_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x2A2_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x2A5_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x2C2_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x2CE_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x2FE_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x319_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x341_po_ctrl_msg1_rx_cb(void);
+extern void C_CAN_0x380_po_ctrl_msg1_rx_cb(void);
+
+extern void PT_CAN_0x3A6_po_ctrl_msg1_rx_cb(void);
+extern void PT_CAN_0x310_pi_display_msg1_tx_cb(void);
+extern void PT_CAN_0x311_pi_display_msg2_tx_cb(void);
+extern void PT_CAN_0x312_pi_display_msg3_tx_cb(void);
+extern void PT_CAN_0x3B5_can1Can_ComTestRec_msg_rx_cb(void);
+extern void RESERVED_CAN_0x2B5_can2Can_ComTestRec_msg_rx_cb(void);
+extern void PT_CAN_0x1B5_can3Can_ComTestRec_msg_rx_cb(void);
+
+extern void PT_CAN_0x300_do_ctrl_msg_rx_cb(void);
+#endif
+
