b2771e7abf909a1275d2d4cf3a3c464d02f0903f
Xjianjian <39685781+Xjianjian@users.noreply.github.com>
2020-09-19 12:15:31 +0800

    Create WKUP.h

---
+++ MCAL/MPC5744P/SYS/WKUP.h
@@ -0,0 +1,16 @@
+/*
+ * WKPU.h
+ *
+ *  Created on: 2020年4月20日
+ 
+ */
+
+#ifndef MCAL_SYS_WKPU_H_
+#define MCAL_SYS_WKPU_H_
+#include "MPC5744P.h"
+#include "platformTypes.h"
+
+extern void wkup_init(void);
+
+
+#endif /* MCAL_SYS_WKPU_H_ */
