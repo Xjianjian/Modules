2c5edbe0826db36fe3e216a784c58eebed829264
Xjianjian <39685781+Xjianjian@users.noreply.github.com>
2020-09-19 11:29:29 +0800

    Create lin_slv.h

---
+++ SERVICE/LIN_COM/LIN_SLAVE/lin_slv.h
@@ -0,0 +1,61 @@
+
+#ifndef LIN_COM_H_
+#define LIN_COM_H_
+
+/*------------------------------------------------------------------------------
+ 					Include files
+------------------------------------------------------------------------------*/
+#include "Lin.h"
+#include "lin_slvCfg.h"
+
+/*------------------------------------------------------------------------------
+                             Defines and constants
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+                                   Data types
+------------------------------------------------------------------------------*/
+
+
+
+
+
+
+/*------------------------------------------------------------------------------
+                      External declaration of global RAM-Variables
+------------------------------------------------------------------------------*/
+
+
+
+
+/*------------------------------------------------------------------------------
+                      External declaration of global ROM-Variables
+------------------------------------------------------------------------------*/
+#if LIN_0_MODULE_ENABLE
+extern const lin_msg_cfg_t lin0_msg_cfg_grp[LIN_COMMCFG_MSG_CNT];
+extern const uint16_t lin0_msg_cnt;
+extern const lin_schedule_t lin0_schedule;
+extern const uint16_t lin0_schedule_item_cnt;
+#endif
+
+
+#if LIN_1_MODULE_ENABLE
+extern const lin_msg_cfg_t lin1_msg_cfg_grp[];
+extern const uint16_t lin1_msg_cnt;
+extern const lin_schedule_t lin1_schedule;
+extern const uint16_t lin1_schedule_item_cnt;
+
+#endif
+/*------------------------------------------------------------------------------
+                    External declaration of global EEPROM-Variables
+------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+                              Declaration of global functions
+------------------------------------------------------------------------------*/
+extern void lin_com_init(void);
+extern void lin_com_task(void);
+
+#endif
+
