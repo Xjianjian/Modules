e48249559fa7d6db9d2524189cbdc8a5beedd800
Xjianjian <39685781+Xjianjian@users.noreply.github.com>
2021-03-01 21:09:59 +0800

    Create autosar_dir_nm_types.h

---
+++ SERVICE/can_nm/autosar_dir_nm_types.h
@@ -0,0 +1,80 @@
+/*
+* Copyright (c) File,SHENZHEN HANGSHENG NEW ENERGY Co.,Ltd.
+* All Rights Reserved.
+* Dept.: Software Department
+* File:autosar_dir_nm_types.h
+* Description: Autosar direct network manager  types header file.
+* REQ IDs:
+* History:
+* Time:2018/5/4
+* User:Devon
+*/
+
+/* ---- Source switch on ------------------------------------------------------*/
+#ifndef AUTOSAR_DIR_NM_TYPES_H_
+#define AUTOSAR_DIR_NM_TYPES_H_
+
+/*------------------------------------------------------------------------------
+ 					Include files
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+                             Defines and constants
+------------------------------------------------------------------------------*/
+
+#ifndef FALSE
+#define FALSE  (0)
+#endif
+
+#ifndef TRUE
+#define TRUE (1)
+#endif
+
+#ifndef NULL_PTR
+#define NULL_PTR 	((void *) 0)
+#endif
+
+#ifndef NULL
+#define NULL		((void) 0)
+#endif
+
+
+/*------------------------------------------------------------------------------
+                                   Data types
+------------------------------------------------------------------------------*/
+typedef unsigned short int nm_word_t;
+typedef unsigned char nm_bool_t;
+typedef unsigned char nm_byte_t;
+typedef unsigned long int nm_dWord_t;
+
+
+
+
+/*------------------------------------------------------------------------------
+                      External declaration of global RAM-Variables
+------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+                      External declaration of global ROM-Variables
+------------------------------------------------------------------------------*/
+
+
+
+
+/*------------------------------------------------------------------------------
+                    External declaration of global EEPROM-Variables
+------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------
+                              Declaration of global functions
+------------------------------------------------------------------------------*/
+
+
+
+#endif
+
+
